99932962dcc2fd7b48181df23e0c1571b7e30d5b
root <root@localhost.localdomain>
2018-09-26 22:49:45 +0800

    add day02zy.py

---
+++ day02.py
@@ -0,0 +1,78 @@
+#EP:1
+'''
+import math
+x1,y1,x2,y2,x3,y3=eval(raw_input(">>"))
+a=math.sqrt(pow((x1-x2),2)+pow((y1-y2),2))
+b=math.sqrt(pow((x1-x3),2)+pow((y1-y3),2))
+c=math.sqrt(pow((x2-x3),2)+pow((y2-y3),2))
+A=math.degrees(math.acos(a*a-b*b-c*c)/(-2*b*c))
+B=math.degrees(math.acos(b*b-a*a-c*c)/(-2*a*c))
+C=math.degrees(math.acos(c*c-b*b-a*a)/(-2*a*b))
+print(A,B,C)
+res='for i in '13290764632@qq.com':
+    res = res + chr(ord(i)) + i
+a='Welcome'+'to'+'Python'
+print(a)
+'''
+# is check "id"
+'''
+a=eval(raw_input(">>"))
+if int(a%2==0):
+    print("oushu")
+else:
+    print("jishu")
+'''
+'''
+people=raw_input(">>"))
+if a=0:
+    print("gun")
+else b=0:
+    print("kaolv")
+if c=
+'''
+
+#EP:4
+import os
+print('start')
+res = input('handsome or no?y/n:>>')
+if res == 'y':
+    res2 = input('money or no?y/n:>>')
+    if res2=='y':
+        res3 = input('wife or no?y/n:>>')
+        if res3=='n':
+            res4 = input('house or no?y/n:>>')
+            if res4=='y':
+                print('meet')
+            else:
+                print('go out')
+        else:
+            print('go out')
+    else:
+        print('think think')
+else:
+    print('go out')
+'''
+import random
+y=random.randint(0,10)
+for i in range(10):
+    x=eval(raw_input(">>"))
+    if(x>y):
+        print("too big")
+    elif(x<y):
+        print("too small")
+    else:
+        print('yeah')
+'''
+#and 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ day02zy.py
@@ -0,0 +1,210 @@
+#1
+'''
+import math
+r=eval(raw_input("Enter the length from the center to a vertex:"))
+s=2*r*(math.sin(math.pi/5))
+Area=(5*s*s)/(4*(math.tan(math.pi/5)))
+print('The area of the pentagon is {:.2f}'.format(Area))
+'''
+#2
+'''
+import math
+x1,y1=eval(raw_input("Enter point 1(latitude and longitude) in degrees:"))
+x2,y2=eval(raw_input("Enter point 2(latitude and longitude) in degrees:"))
+radius=6371.01
+x1=math.radians(x1)
+x2=math.radians(x2)
+y1=math.radians(y1)
+y2=math.radians(y2)
+part1=math.acos(math.sin(x1)*math.sin(x2)+math.cos(x1)*math.cos(x2)*math.cos(y1-y2))
+d=part1*radius
+print("The distance between the two points is {} km ".format(d))
+'''
+#3
+'''
+import math
+s=eval(raw_input("Enter the side:"))
+Area=(5*s*s)/(4*math.tan(math.pi/5))
+print("The area of the pentagon is {}".format(Area))
+'''
+#4
+'''
+import math
+n=eval(raw_input("Enter the number of sides:"))
+s=eval(raw_input("Enter the side:"))
+part1=n*s*s
+part2=4*math.tan(math.pi/n)
+Area=part1/part2
+print("The area of the polygn is {}".format(Area))
+'''
+#5
+'''
+x=eval(raw_input("Enter an ASCII code:"))
+y=chr(x)
+print("The character is {}".format(y))
+'''
+#6
+'''
+name=str(raw_input("Enter employee's name:"))
+hours=eval(raw_input("Enter number of hours worked in a week:"))
+pay=eval(raw_input("Enter hourly pay rate:"))
+ftax=eval(raw_input("Enter federal tax withholding rate:"))
+stax=eval(raw_input("Enter state tax withholding rate:"))
+G=hours*pay
+F=G*ftax
+S=G*stax
+N=G-F-S
+print("Employee Name:{}".format(name))
+print("Hours Worked:{}".format(hours))
+print("Pay Rate:${}".format(pay))
+print("Gross Pay:${}".format(G))
+print("Deductions:")
+print("  Federal Withholding(20.0%):${}".format(F))
+print("  State Withholding(9.0%):${:.2f}".format(S))
+print("  Total Deduction:${:.2f}".format(F+S))
+print("  Net Pay:${:.2f}".format(N))
+'''
+#7
+'''
+number=int(raw_input("Enter an integer:"))
+a=number/1000
+b=(number-(a*1000))/100
+c=(number-(a*1000)-(b*100))/10
+d=number-a*1000-b*100-c*10
+number2=d*1000+c*100+b*10+a
+print("The reversed number is{}".format(number2))
+'''
+#8
+'''
+res=' '
+for i in "1329076463@qq.com":
+    res=res+chr(ord(i)+2)
+print(res)
+'''
+
+#9
+'''
+import math
+a,b,c=eval(raw_input("Enter a,b,c:"))
+if (b*b-4*a*c)>0:
+    r1=((-b)+math.sqrt(b*b-4*a*c))/(2*a)
+    r2=((-b)-math.sqrt(b*b-4*a*c))/(2*a)
+    print("The roots are{:.6f} and {:.5f} ".format(r1,r2))
+elif (b*b-4*a*c)==0:
+    r1=((-b)+math.sqrt(b*b-4*a*c))/(2*a)
+    print("The root are {}".format(r1))
+else:
+    print("The equation has no real roots")
+'''
+#10
+'''
+import random
+num1=random.randrange(0,100)
+num2=random.randrange(0,100)
+print("num1:{},num2:{}".format(num1,num2))
+x=eval(raw_input("Enter a sum:"))
+s=num1+num2
+if x==s:
+    print("True")
+else:
+    print("False")
+'''
+#11
+import math
+number1=eval(raw_input"Enter today's day:")
+number2=eval(raw_input"Enter the number of days elapsed since today:")
+def today(n):
+    elif n==0:
+        return 'Sunday'
+    elif n==1:
+        return 'Monday'
+    elif n==2:
+        return 'Tuesday'
+    elif n==3:
+        return 'Wednesday'
+    elif n==4:
+        return 'Thursday'
+    elif n==5:
+        return 'Friday'
+    elif n==6:
+        return 'Saturday'
+s=number2%7
+print("Today is {} and the future day is {}".format(today(number1),today(s)))
+#12
+'''
+a,b,c=eval(raw_input("enter three number:"))
+if a>b:
+    a,b=b,a
+if a>c:
+    a,c=c,a
+if b>c:
+    b,c=c,b
+print(a,b,c)
+'''
+#13
+'''
+price1,weight1=eval(raw_input("Enter weight and price for package 1:"))
+price2,weight2=eval(raw_input("Enter weight and price for package 2:"))
+a=price1/weight1
+b=price2/weight2
+if a>b:
+    print("Package 2 has the better price.")
+else:
+    print("Package 1 has the better price.")
+'''
+#14
+
+#15
+'''
+import random
+num=random.randint(0,1)
+x=eval(raw_input("enter  0 or 1:"))
+if x==num:
+    print("True")
+else:
+    print("False")
+'''
+#16
+'''
+import random
+num=random.randint(0,1,2)
+x=eval(raw_input("scissor(0),rock(1),paper(2):"))
+if num==0:
+    '''
+#17
+'''
+import random
+a=random.choice(['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'])
+b=random.choice(['flower','redpeach','square','blackpeach'])
+print("The card you picked is the {} of {}".format(a,b))
+'''
+#18
+'''
+import math
+number=eval(raw_input("Enter a three-digit:"))
+a=number/100
+b=number%10
+if(a==b):
+    print("{} is a palindrome".format(number))
+else:
+    print("{} is not a palindrome".format(number))
+'''
+#19
+'''
+import math
+a,b,c=eval(raw_input("Enter three edges:"))
+x=a+b
+y=b+c
+z=a+c
+if x>c and y>a and z>b:
+    print("The perimeter is:{}".format(a+b+c))
+else:
+    print("False")
+'''
+
+
+
+
+
+
+
